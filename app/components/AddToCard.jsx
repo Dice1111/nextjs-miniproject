abc7a67e9ec25e02c7c17f269034e8fa3c724fd5
Dice <98948753+Dice1111@users.noreply.github.com>
2024-10-09 14:46:38 +0800

    init

---
+++ app/components/AddToCard.jsx
@@ -0,0 +1,15 @@
+
+'use client';
+
+
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <div>
+        <button onClick={()=>console.log("apple")}>Click here</button>
+    </div>
+  )
+}
+
+export default AddToCard
